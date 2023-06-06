c4ad715898954976aa29a72460577836c21f2b05
EGalanin <99761893+EGalanin@users.noreply.github.com>
2023-06-06 18:00:32 +0300

    first commit

---
+++ src/js/__test__/test.js
@@ -0,0 +1,54 @@
+import dectructuring from '../app';
+
+test('sorting', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  };
+  expect(dectructuring(character)).toEqual(
+    [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        description: 'Описание недоступно',
+      },
+    ],
+  );
+});
+
+test('Special -> []', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+  };
+  expect(dectructuring(character)).toEqual([]);
+});
